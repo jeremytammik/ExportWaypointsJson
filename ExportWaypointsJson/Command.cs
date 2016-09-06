86d9c33b9ba75d1674eeb7be3c575c9a5deb9ece
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-09-06 09:48:36 +0200

    added Newtonsoft.Json nuget package

--- ExportWaypointsJson/Command.cs
+++ ExportWaypointsJson/Command.cs
@@ -7,6 +7,7 @@ using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
+using Newtonsoft.Json;
 #endregion
 
 namespace ExportWaypointsJson
